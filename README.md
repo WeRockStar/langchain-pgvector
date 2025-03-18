c699f5ca75085f78821a39441fef148faf91fd0d
WeRockStar <kotchaphan.m@gmail.com>
2025-03-18 22:24:27 +0700

    Add how to enable pgvector extension

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Learning How to Work with pgvector
+
+## Introduction
+
+This is a playground for me to learn how to work with `pgvector`.
+
+## pgvector
+
+- Enable the extension:
+	```sql
+	CREATE EXTENSION IF NOT EXISTS vector;
+	```
+- Create `documents` table and we will use `embedding` column as a vector of 1024 dimensions:
+	```sql
+	CREATE TABLE IF NOT EXISTS documents (id SERIAL PRIMARY KEY, content TEXT, embedding vector(1024));
+	```
+	Note: Dimension of the vector can be changed as per the requirement of precision we need.
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import List
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import PGVector
+from langchain_postgres.vectorstores import PGVector
 from langchain_core.documents import Document
 from langchain_community.embeddings import OpenAIEmbeddings
 import os
@@ -14,7 +14,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 embeddings = (
-    OllamaEmbeddings(model="llama3.2") if os.getenv("MODEL") == "ollama" else OpenAIEmbeddings()
+    OllamaEmbeddings(model="llama3.2")
+    if os.getenv("MODEL") == "ollama"
+    else OpenAIEmbeddings(model="text-embedding-3-large")
 )
 llm = ChatOllama(model="llama3.2") if os.getenv("MODEL") == "ollama" else ChatOpenAI()
 
@@ -33,23 +35,22 @@ def load_docs() -> List[Document]:
     return documents
 
 
-def indexting():
+def indexting() -> None:
     documents = load_docs()
 
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
 
-    connection_string = "postgresql+psycopg2://postgres:postgres@localhost:5432/songs"
+    connection_string = "postgresql+psycopg://postgres:postgres@localhost:5432/songs"
 
-    db = PGVector.from_documents(
-        embedding=embeddings,
-        documents=docs,
-        connection_string=connection_string,
+    vector_store = PGVector(
+        embeddings=embeddings,
         collection_name="songs",
+        connection=connection_string,
+        use_jsonb=True,
     )
 
-    db.add_documents(docs)
-    return db
+    vector_store.add_documents(docs)
 
 
 if __name__ == "__main__":
