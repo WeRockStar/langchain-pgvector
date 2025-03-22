63b5e235816ffae0148c705a53e1d4574fdf0f47
WeRockStar <kotchaphan.m@gmail.com>
2025-03-22 13:35:14 +0700

    Enhance indexing and add search example

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import Logger
 from pathlib import Path
 from typing import List
 from langchain.text_splitter import CharacterTextSplitter
@@ -35,23 +36,41 @@ def load_docs() -> List[Document]:
     return documents
 
 
-def indexting() -> None:
+def indexing() -> None:
     documents = load_docs()
 
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
 
     connection_string = "postgresql+psycopg://postgres:postgres@localhost:5432/songs"
-
+    print("Indexing in progress...")
     vector_store = PGVector(
         embeddings=embeddings,
-        collection_name="songs",
+        collection_name="documents",
         connection=connection_string,
         use_jsonb=True,
+        logger=Logger(__name__),
     )
-
     vector_store.add_documents(docs)
+    print("Indexing completed.")
+
+
+def search(queury: str) -> None:
+    print(f"Use {queury} to search")
+    connection_string = "postgresql+psycopg://postgres:postgres@localhost:5432/songs"
+    vector_store = PGVector(
+        embeddings=embeddings,
+        collection_name="documents",
+        connection=connection_string,
+        use_jsonb=True,
+        logger=Logger(__name__),
+    )
+    results = vector_store.similarity_search(query, k=2)
+    for result in results:
+        print(result.page_content)
 
 
 if __name__ == "__main__":
-    indexting()
+    indexing()
+    query = input("Enter your query: ")
+    search(query)
